a1f277678c697c749c85ff9cccda71c0c1418b6e
Paul Epps <paul@eppsnet.com>
2020-06-12 13:02:51 -0700

    First commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,15 +7,15 @@ const Header = () => {
   return (
     <header>
       <Container type="content">
-        <p>My Gatsby Site</p>
-        <ul>
+        <p>COVID-19 Tracker</p>
+        {/* <ul>
           <li>
             <Link to="/">Home</Link>
           </li>
           <li>
             <Link to="/page-2/">Page 2</Link>
           </li>
-        </ul>
+        </ul> */}
       </Container>
     </header>
   );
