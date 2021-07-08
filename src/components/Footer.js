dfbb9cef9aa97a5724be219f1f61a233341547be
Paul Epps <paul@eppsnet.com>
2021-07-07 21:02:58 -0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer>
       <Container>
-        <p>&copy; { new Date().getFullYear() }, My Gatsby Site</p>
+        <p>&copy; { new Date().getFullYear() }, PE Gatsby Site</p>
       </Container>
     </footer>
   );
